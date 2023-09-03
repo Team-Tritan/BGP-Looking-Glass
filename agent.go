4acd18dbd1159c742602c8b3ff6320091364a15b
Dylan <dylan@tritan.dev>
2023-09-03 04:44:39 -0400

    i have no idea if this will work

---
+++ agent.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func executeCommand(command string) (string, error) {
+	parts := strings.Fields(command)
+	cmd := exec.Command(parts[0], parts[1:]...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/route", func(c *fiber.Ctx) error {
+		ip := c.Query("ip")
+		if ip == "" {
+			return c.Status(fiber.StatusBadRequest).SendString("IP parameter is required")
+		}
+		cmd := fmt.Sprintf("sudo birdc show route %s", ip)
+		response, err := executeCommand(cmd)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		}
+		return c.SendString(fmt.Sprintf("Route Info for IP %s:\n%s", ip, response))
+	})
+
+	app.Get("/mtr", func(c *fiber.Ctx) error {
+		ip := c.Query("ip")
+		if ip == "" {
+			return c.Status(fiber.StatusBadRequest).SendString("IP parameter is required")
+		}
+		cmd := fmt.Sprintf("mtr %s", ip)
+		response, err := executeCommand(cmd)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		}
+		return c.SendString(fmt.Sprintf("MTR for IP %s:\n%s", ip, response))
+	})
+
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		ip := c.Query("ip")
+		if ip == "" {
+			return c.Status(fiber.StatusBadRequest).SendString("IP parameter is required")
+		}
+		cmd := fmt.Sprintf("ping %s", ip)
+		response, err := executeCommand(cmd)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		}
+		return c.SendString(fmt.Sprintf("Ping for IP %s:\n%s", ip, response))
+	})
+
+	app.Get("/traceroute", func(c *fiber.Ctx) error {
+		ip := c.Query("ip")
+		if ip == "" {
+			return c.Status(fiber.StatusBadRequest).SendString("IP parameter is required")
+		}
+		cmd := fmt.Sprintf("traceroute %s", ip)
+		response, err := executeCommand(cmd)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		}
+		return c.SendString(fmt.Sprintf("Traceroute for IP %s:\n%s", ip, response))
+	})
+
+	app.Get("/bgp", func(c *fiber.Ctx) error {
+		asn := c.Query("asn")
+		if asn == "" {
+			return c.Status(fiber.StatusBadRequest).SendString("ASN parameter is required")
+		}
+		cmd := fmt.Sprintf("sudo birdc show route where bgp_path ~ [= * %s * =] all", asn)
+		response, err := executeCommand(cmd)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		}
+		return c.SendString(fmt.Sprintf("BGP Routes for ASN %s:\n%s", asn, response))
+	})
+
+	log.Fatal(app.Listen(":8080"))
+}
