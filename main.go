5d7e623e56433ca38decf172c0721a48f585fed5
Dylan <dylan@tritan.dev>
2023-09-03 06:53:17 -0400

    best

--- main.go
+++ main.go
@@ -29,49 +29,49 @@ func main() {
 	app.Get("/show-route", func(c *fiber.Ctx) error {
 		subnet := c.Query("subnet")
 		if !regex.IsValidSubnet(subnet) {
-			return c.Status(fiber.StatusBadRequest).SendString("Invalid subnet param")
+			return c.Status(fiber.StatusBadRequest).SendString("~as393577 looking glass (ง'̀-'́)ง♡~\n\nInvalid subnet param")
 		}
 		response, err := commands.ExecuteBirdCommand(subnet)
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 		}
-		return c.SendString(fmt.Sprintf("Route Info for IP %s:\n%s", subnet, response))
+		return c.SendString(fmt.Sprintf("~as393577 looking glass (ง'̀-'́)ง♡~\n\nRoute Info for IP %s:\n%s", subnet, response))
 	})
 
 	app.Get("/asn-routes", func(c *fiber.Ctx) error {
 		asn := c.Query("asn")
 		if !regex.IsValidASN(asn) {
-			return c.Status(fiber.StatusBadRequest).SendString("Invalid ASN param")
+			return c.Status(fiber.StatusBadRequest).SendString("~as393577 looking glass (ง'̀-'́)ง♡~\n\nInvalid ASN param")
 		}
 		response, err := commands.ExecuteBirdCommand(fmt.Sprintf("where bgp_path ~ [= * %s * =] all", asn))
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 		}
-		return c.SendString(fmt.Sprintf("BGP Routes for ASN %s:\n%s", asn, response))
+		return c.SendString(fmt.Sprintf("~as393577 looking glass (ง'̀-'́)ง♡~\n\nBGP Routes for ASN %s:\n%s", asn, response))
 	})
 
 	app.Get("/ping", func(c *fiber.Ctx) error {
 		ip := c.Query("ip")
 		if !regex.IsValidIP(ip) {
-			return c.Status(fiber.StatusBadRequest).SendString("Invalid IP param")
+			return c.Status(fiber.StatusBadRequest).SendString("~as393577 looking glass (ง'̀-'́)ง♡~\n\nInvalid IP param")
 		}
 		response, err := commands.ExecutePing(ip)
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 		}
-		return c.SendString(fmt.Sprintf("Ping for IP %s:\n%s", ip, response))
+		return c.SendString(fmt.Sprintf("~as393577 looking glass (ง'̀-'́)ง♡~\n\nPing for IP %s:\n%s", ip, response))
 	})
 
 	app.Get("/traceroute", func(c *fiber.Ctx) error {
 		ip := c.Query("ip")
 		if !regex.IsValidIP(ip) {
-			return c.Status(fiber.StatusBadRequest).SendString("Invalid IP param")
+			return c.Status(fiber.StatusBadRequest).SendString("~as393577 looking glass (ง'̀-'́)ง♡~\n\nInvalid IP param")
 		}
 		response, err := commands.ExecuteTraceroute(ip)
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 		}
-		return c.SendString(fmt.Sprintf("Traceroute for IP %s:\n%s", ip, response))
+		return c.SendString(fmt.Sprintf("~as393577 looking glass (ง'̀-'́)ง♡~\n\nTraceroute for IP %s:\n%s", ip, response))
 	})
 
 	log.Fatal(app.Listen(":4000"))
