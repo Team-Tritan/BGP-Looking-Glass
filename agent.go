d7af5c9a7f5ea01206546eeaf879c4d97c6472d1
Dylan <dylan@tritan.dev>
2023-09-03 05:08:58 -0400

    ok

--- agent.go
+++ agent.go
@@ -9,6 +9,16 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type RouteInfoResponse struct {
+	IP       string `json:"ip"`
+	Response string `json:"response"`
+}
+
+type BGPRouteResponse struct {
+	ASN      string `json:"asn"`
+	Response string `json:"response"`
+}
+
 func executeCommand(command string) (string, error) {
 	parts := strings.Fields(command)
 	cmd := exec.Command(parts[0], parts[1:]...)
@@ -22,43 +32,43 @@ func executeCommand(command string) (string, error) {
 func main() {
 	app := fiber.New()
 
-	app.Get("/route", func(c *fiber.Ctx) error {
+	app.Get("/routes/show", func(c *fiber.Ctx) error {
 		ip := c.Query("ip")
 		if ip == "" {
-			return c.Status(fiber.StatusBadRequest).SendString("IP parameter is required")
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "IP parameter is required"})
 		}
-		cmd := fmt.Sprintf("birdc show route %s", ip)
+		cmd := fmt.Sprintf("sudo birdc show route %s", ip)
 		response, err := executeCommand(cmd)
 		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 		}
-		return c.SendString(fmt.Sprintf("Route Info for IP %s:\n%s", ip, response))
+		return c.Status(fiber.StatusOK).JSON(RouteInfoResponse{IP: ip, Response: response})
+	})
+
+	app.Get("/routes/bgp", func(c *fiber.Ctx) error {
+		asn := c.Query("asn")
+		if asn == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "ASN parameter is required"})
+		}
+		cmd := fmt.Sprintf("sudo birdc show route where bgp_path ~ [= * %s * =] all", asn)
+		response, err := executeCommand(cmd)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		}
+		return c.Status(fiber.StatusOK).JSON(BGPRouteResponse{ASN: asn, Response: response})
 	})
 
 	app.Get("/ping", func(c *fiber.Ctx) error {
 		ip := c.Query("ip")
 		if ip == "" {
-			return c.Status(fiber.StatusBadRequest).SendString("IP parameter is required")
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "IP parameter is required"})
 		}
 		cmd := fmt.Sprintf("ping %s", ip)
 		response, err := executeCommand(cmd)
 		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 		}
-		return c.SendString(fmt.Sprintf("Ping for IP %s:\n%s", ip, response))
-	})
-
-	app.Get("/routes/asn", func(c *fiber.Ctx) error {
-		asn := c.Query("asn")
-		if asn == "" {
-			return c.Status(fiber.StatusBadRequest).SendString("ASN parameter is required")
-		}
-		cmd := fmt.Sprintf("sudo birdc show route where bgp_path ~ [= * %s * =] all", asn)
-		response, err := executeCommand(cmd)
-		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
-		}
-		return c.SendString(fmt.Sprintf("BGP Routes for ASN %s:\n%s", asn, response))
+		return c.Status(fiber.StatusOK).JSON(RouteInfoResponse{IP: ip, Response: response})
 	})
 
 	log.Fatal(app.Listen(":8080"))
