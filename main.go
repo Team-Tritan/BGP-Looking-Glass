d80ab5e185ad8b31c88b5459b1de53939c39602e
xfy <135300152+xfy777@users.noreply.github.com>
2023-09-05 08:14:11 -0400

    Update main.go

--- main.go
+++ main.go
@@ -2,30 +2,31 @@ package main
 
 import (
 	"log"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"tritan.dev/bgp-tool/handlers"
 )
 
 func main() {
-	app := fiber.New()
-
-	app.Use(cors.New(cors.Config{
-		AllowOrigins: "*",
-		AllowMethods: "GET,POST,PUT,DELETE",
-		AllowHeaders: "Origin, Content-Type, Accept",
-	}))
-
-	SetupRoutes(app)
-
+	app := setupApp()
 	err := app.Listen(":4000")
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func SetupRoutes(app *fiber.App) {
+func setupApp() *fiber.App {
+	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST,PUT,DELETE",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+	setupRoutes(app)
+	return app
+}
+
+func setupRoutes(app *fiber.App) {
 	app.Get("/", handlers.Landing)
 	app.Get("/show-route", handlers.ShowRoute)
 	app.Get("/bgp-routes", handlers.BgpRoutes)
@@ -33,3 +34,4 @@ func SetupRoutes(app *fiber.App) {
 	app.Get("/traceroute", handlers.Traceroute)
 	app.Get("/mtr", handlers.Mtr)
 }
+
